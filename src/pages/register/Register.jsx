7ad19cffcce602432d6c147a30109a80c5aa5aef
onlineiqbal <onlineiqbal@yahoo.co.in>
2022-12-27 16:49:48 +1030

    still struggling with path

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -23,7 +23,7 @@ export default function Register() {
         password: password.current.value,
       };
       try {
-        await axios.post("/auth/register", user);
+        await axios.post("https://node-rest-api-soqw.onrender.com/api/auth/register", user);
       //  history.push("/login");
       navigate("/login");
       } catch (err) {
